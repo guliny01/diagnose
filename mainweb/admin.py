989eef965633c982ed0efc6cdd4ee1349f985287
guliny01 <19483834@qq.com>
2017-11-28 10:27:18 +0800

    first commit

---
+++ mainweb/admin.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Product,Domain,Domain_ip,Server,Server_type,Area_type,Domain_idc,Domain_check,Progress_type,Progress,Config_type,Config,Alter
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','area_type','description','contact')
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('product','domain','slb_vhost','slb_cluster','domain_url','respone_way')
+
+class Domain_ipAdmin(admin.ModelAdmin):
+    list_display = ('name','ip','description','status')
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('product','server','description','public_ip','private_ip','idc_location','status','url_path')
+
+class Server_typeAdmin(admin.ModelAdmin):
+    list_display = ('server_name','description')
+
+class Area_typeAdmin(admin.ModelAdmin):
+    list_display = ('area_name','description')
+
+class Domain_idcAdmin(admin.ModelAdmin):
+    list_display = ('id','idc')
+
+class Domain_checkAdmin(admin.ModelAdmin):
+    list_display = ('domain_name','api_abstract','api_description','api_url')
+
+class Progress_typeAdmin(admin.ModelAdmin):
+    list_display = ('progress_name','path','config_name')
+
+class Progress_Admin(admin.ModelAdmin):
+    list_display = ('description','comment')
+
+class Config_typeAdmin(admin.ModelAdmin):
+    list_display = ('config_type_name','description')
+
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = ('description','config_path','config_content')
+
+class AlterAdmin(admin.ModelAdmin):
+    list_display = ('alter_name','alter_content','alter_time')
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Domain,DomainAdmin)
+admin.site.register(Domain_ip,Domain_ipAdmin)
+admin.site.register(Server,ServerAdmin)
+admin.site.register(Server_type,Server_typeAdmin)
+admin.site.register(Area_type,Area_typeAdmin)
+admin.site.register(Domain_idc,Domain_idcAdmin)
+admin.site.register(Domain_check,Domain_checkAdmin)
+admin.site.register(Progress_type,Progress_typeAdmin)
+admin.site.register(Progress,Progress_Admin)
+admin.site.register(Config_type,Config_typeAdmin)
+admin.site.register(Config,ConfigAdmin)
+admin.site.register(Alter,AlterAdmin)
---
+++ mainweb/models.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+import django.utils.timezone as timezone
+import datetime
+# Create your models here.
+
+class Area_type(models.Model):
+    area_name = models.CharField(max_length=60)
+    description = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.area_name
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    area_type = models.ForeignKey(Area_type,on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    contact = models.CharField(max_length=200)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.name
+
+
+
+class Domain(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    domain = models.CharField(max_length=100)
+    slb_vhost = models.CharField(max_length=100)
+    vhost_url = models.CharField(max_length=100,default='/')
+    cname = models.CharField(max_length=100)
+    slb_cluster = models.CharField(max_length=100)
+    domain_url = models.CharField(max_length=200,default='/')
+    respone_type = (
+        ('Http','Http'),
+        ('Https','Https'),
+    )
+    respone_way = models.CharField(max_length=5,choices=respone_type)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.domain
+
+class Domain_idc(models.Model):
+    idc = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.idc
+
+class Domain_ip(models.Model):
+    name = models.ForeignKey(Domain,on_delete=models.CASCADE)
+    ip = models.CharField(max_length=100)
+    #description = models.CharField(max_length=100)
+    description = models.ForeignKey(Domain_idc,on_delete=models.CASCADE)
+    status_type = (
+        ('Valid','Valid'),
+        ('Invalid','Invalid'),
+    )
+    status = models.CharField(max_length=10,choices=status_type)
+    option = models.CharField(max_length=10,default='0')
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.ip
+
+class Domain_check(models.Model):
+    domain_name = models.ForeignKey(Domain,related_name='domain_name',on_delete=models.CASCADE)
+    api_abstract = models.CharField(max_length=50)
+    api_description = models.CharField(max_length=100)
+    api_url = models.CharField(max_length=200)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.api_abstract
+
+class Server_type(models.Model):
+    server_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    server_config_name = models.CharField(max_length=100,default='')
+    server_config_path = models.CharField(max_length=100,default='')
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.server_name
+
+class Server(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    server = models.ForeignKey(Server_type,on_delete=models.CASCADE)
+    description = models.CharField(max_length=100,default='')
+    vip = models.CharField(max_length=100,default='127.0.0.1')
+    public_ip = models.CharField(max_length=100)
+    private_ip = models.CharField(max_length=100)
+    status_type = (
+        ('Valid','Valid'),
+        ('Invalid','Inalid'),
+    )
+    idc_location = models.CharField(max_length=100)
+    port = models.CharField(max_length=10,default='80')
+    url_path = models.CharField(max_length=100,default='/')
+    option = models.CharField(max_length=10,default='0')
+    status = models.CharField(max_length=5,choices=status_type)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.description
+
+class Progress_type(models.Model):
+    progress_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    path = models.CharField(max_length=100)
+    content = models.TextField(default='')
+    config_name = models.CharField(max_length=100)
+    config_content = models.TextField(default='')
+    def __unicode__(self):
+        return self.description
+
+class Progress(models.Model):
+    product_rel = models.ForeignKey(Product,related_name='product_rel',on_delete=models.CASCADE)
+    server_rel = models.ForeignKey(Server_type,related_name='server_rel',on_delete=models.CASCADE)
+    progress_rel = models.ForeignKey(Server_type,related_name='progress_rel',on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.description
+
+class Config_type(models.Model):
+    config_type_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.config_name
+
+class Config(models.Model):
+    config_name_rel = models.ForeignKey(Config_type,related_name='config_rel',on_delete=models.CASCADE)
+    config_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    config_content = models.TextField(default='')
+    config_path = models.CharField(max_length=100)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.description
+
+class Alter(models.Model):
+    alter_name = models.CharField(max_length=100)
+    alter_content = models.TextField(default='')
+    alter_time = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(default='暂无说明')
+    def __unicode__(self):
+        return self.alter_name
+
