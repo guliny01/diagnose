85b46c57f40c5d96661566b47874ba0f373dae2d
guliny01 <19483834@qq.com>
2017-11-30 10:12:47 +0800

    数据结构第一次调整

--- diagnose/settings.py
+++ diagnose/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Diagnose',
         'USER': 'web',
-        'PASSWORD': '123456',
+        'PASSWORD': 'letvletv1001',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
